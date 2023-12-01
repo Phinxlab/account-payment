92aafb4ec2974701e6440ea154f538febbb25a80
milton-phinx <milton.guzman@phinxlab.com.ar>
2023-12-01 14:45:27 +0000

    [UPD] Rollback de actualizaciones account_payment_group

--- l10n_latam_check/models/account_journal.py
+++ l10n_latam_check/models/account_journal.py
@@ -9,15 +9,21 @@ class AccountJournal(models.Model):
         inverse='_inverse_l10n_latam_use_checkbooks', copy=False)
     l10n_latam_checkbook_ids = fields.One2many('l10n_latam.checkbook', 'journal_id', 'Checkbooks')
 
-    @api.model
-    def _get_checkbooks_by_default_country_codes(self):
-        """ Return the list of country codes for the countries where using checkbooks is enable by default """
-        return ["AR"]
+    def _default_outbound_payment_methods(self):
+        if self._context.get('third_checks_journal'):
+            return self.env.ref('l10n_latam_check.account_payment_method_out_third_checks')
+        return super()._default_outbound_payment_methods()
+
+    def _default_inbound_payment_methods(self):
+        if self._context.get('third_checks_journal'):
+            return self.env.ref('l10n_latam_check.account_payment_method_new_third_checks') + \
+                self.env.ref('l10n_latam_check.account_payment_method_in_third_checks')
+        return super()._default_inbound_payment_methods()
 
     @api.depends('outbound_payment_method_line_ids', 'company_id.country_id.code', 'check_manual_sequencing')
     def _compute_l10n_latam_use_checkbooks(self):
         arg_checks = self.filtered(
-                lambda x: not x.check_manual_sequencing and x.company_id.country_id.code in self._get_checkbooks_by_default_country_codes() and
+                lambda x: not x.check_manual_sequencing and x.company_id.country_id.code == 'AR' and
                 'check_printing' in x.outbound_payment_method_line_ids.mapped('code'))
         arg_checks.l10n_latam_use_checkbooks = True
         # disable checkbook if manual sequencing was enable
